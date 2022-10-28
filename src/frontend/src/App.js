4e80dc698b4b6ba2761bcb3ebccfdc74780cc8a7
e1zdr <ivan.zdorovets@gmail.com>
2022-10-28 15:32:21 +0300

    FE basic chat

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,23 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useCallback, useState } from "react";
+
+import "./App.css";
 
 function App() {
+  const messageArray = [
+    {
+      message: "Some sample message 1",
+      from: { username: "John Dou" },
+      to: { username: "Some User" },
+      type: "incoming",
+    },
+    {
+      message: "Some sample message 2",
+      from: { username: "John Dou" },
+      to: { username: "Some User" },
+      type: "outgoing",
+    },
+    {
+      message: "Some sample message 3",
+      from: { username: "John Dou" },
+      to: { username: "Some User" },
+      type: "incoming",
+    },
+    {
+      message: "Some sample message 4",
+      from: { username: "John Dou" },
+      to: { username: "Some User" },
+      type: "incoming",
+    },
+    {
+      message: "Some sample message 5",
+      from: { username: "John Dou" },
+      to: { username: "Some User" },
+      type: "outgoing",
+    },
+  ];
+
+  const [messages, setMessages] = useState(messageArray);
+  const [message, setMessage] = useState("");
+
+  const divRef = useRef(null);
+
+  const onChangeMessage = useCallback((e) => {
+    setMessage(e.target.value);
+  }, []);
+
+  const onClickSend = useCallback(() => {
+    if (message) {
+      setMessages([
+        ...messages,
+        {
+          message,
+          from: { username: "John Dou" },
+          to: { username: "Some User" },
+          type: "outgoing",
+        },
+      ]);
+      setMessage("");
+    }
+  }, [message, messages]);
+
+  useEffect(() => {
+    divRef.current.scrollIntoView();
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Title">Sample Teams chat</div>
+      <div className="ChatWrap">
+        <div className="MessagesBox">
+          {messages.map((e, i) =>
+            e.type === "incoming" ? (
+              <div key={i} className="Message">
+                <div>
+                  <div className="Username">{e.from.username}</div>
+                  <div>{e.message}</div>
+                </div>
+              </div>
+            ) : (
+              <div key={i} className="Message Message_Outgoing">
+                <div className="MessageWrap">
+                  <div className="Username">{e.to.username}(you)</div>
+                  <div>{e.message}</div>
+                </div>
+              </div>
+            )
+          )}
+          <div ref={divRef} />
+        </div>
+        <div className="MessagesSend">
+          <textarea value={message} onChange={onChangeMessage} className="Textarea" />
+          <button onClick={onClickSend} className="SendButton">
+            Send
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
