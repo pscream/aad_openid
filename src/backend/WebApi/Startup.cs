fe518d8d583579ee96beddc03d6c7b797d85b5d0
Kostia <KostiaChorny@users.noreply.github.com>
2022-10-28 13:52:52 +0300

    Disable https

--- src/backend/WebApi/Startup.cs
+++ src/backend/WebApi/Startup.cs
@@ -55,10 +55,10 @@ namespace WebApi
             {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                //app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
